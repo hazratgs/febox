9e1a416d681bde9c8d91ffc2fa04b74b266a8c3d
hazratgs <hazratgs@gmail.com>
2017-02-27 00:59:49 +0300

    Анимация компонента Aside

--- src/components/Aside/index.js
+++ src/components/Aside/index.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react'
 import { Link } from 'react-router'
 import s from './styles.css'
 
+import VkIcon from './vk.svg'
+
 export default class Aside extends Component {
+
+    state = {
+        navLoader: 'hidden',
+        socialLoader: 'hidden',
+        contactLoader: 'hidden'
+    }
 
     hideAside = (e) => {
         if (e.target.classList.contains(s.aside)){
             this.props.handleHideAside();
         }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        setTimeout(() => this.setState({navLoader: nextProps.state}), 200)
+        setTimeout(() => this.setState({socialLoader: nextProps.state}), 270)
+        setTimeout(() => this.setState({contactLoader: nextProps.state}), 340)
     }
 
     render() {
@@ -16,26 +30,43 @@ export default class Aside extends Component {
                 className={`${s.aside} ${s[this.props.state]}`}
                 onClick={this.hideAside}>
                 <div className={s.wrapper}>
-                    <div className={s.nav}>
-                        <div className={s.item}>
-                            <Link
-                                to='/works'
-                                activeClassName={s.active}
-                                onClick={this.props.handleHideAside}
-                            >портфолио</Link>
+                    <div className={s.scroll}>
+                        <div className={`${s.nav} ${s[this.state.navLoader]}`}>
+                            <div className={s.item}>
+                                <Link
+                                    to='/works'
+                                    activeClassName={s.active}
+                                    onClick={this.props.handleHideAside}
+                                >портфолио</Link>
+                            </div>
+                            <div className={s.item}>
+                                <Link
+                                    to='/company'
+                                    activeClassName={s.active}
+                                    onClick={this.props.handleHideAside}
+                                >студия</Link>
+                            </div>
+                            <div className={s.item}>
+                                <Link to='/contact'
+                                      activeClassName={s.active}
+                                      onClick={this.props.handleHideAside}
+                                >контакты</Link>
+                            </div>
                         </div>
-                        <div className={s.item}>
-                            <Link
-                                to='/company'
-                                activeClassName={s.active}
-                                onClick={this.props.handleHideAside}
-                            >студия</Link>
+                        <div className={`${s.social} ${s[this.state.socialLoader]}`}>
+                            <a href='/'>
+                                <VkIcon/>
+                                <span>Мы Вконтакте</span>
+                            </a>
                         </div>
-                        <div className={s.item}>
-                            <Link to='/contact'
-                                  activeClassName={s.active}
-                                  onClick={this.props.handleHideAside}
-                            >контакты</Link>
+                        <div className={`${s.contact} ${s[this.state.contactLoader]}`}>
+                            <div className={s.phone}>
+                                <a href='mailto:hello@febox.ru'>
+                                    <span>hello@febox.ru</span>
+                                </a>
+                                <a href='tel:+79285396966'>+7 (883) 211-80-80</a>
+                            </div>
+                            <div className={s.address}>Дербент, Гагарина, 15 А, 2 этаж</div>
                         </div>
                     </div>
                 </div>
