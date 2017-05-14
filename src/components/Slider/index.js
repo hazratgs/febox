66816eb137cf3f6fcc6b21855a3eeb3a42cd3d7c
hazratgs <hazratgs@gmail.com>
2017-05-15 01:18:06 +0300

    Видео на главный слайд, обеспечена работа на мобильных устройствах

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import ReactDriveIn from 'react-drive-in'
+import ReactPlayer from 'react-player'
 import { Link } from 'react-router'
 import s from './styles.css'
 
@@ -27,12 +27,14 @@ export default class Slider extends Component {
             <div className={s.wrapper}>
                 <div className={s.item}>
                     <div className={`${s.video} ${s[this.state.video]}`}>
-                        <ReactDriveIn
-                            // show='http://avantel.ru/upload/iblock/de9/_7.webm'
-                             show='http://avantel.ru/upload/iblock/cc6/_4.webm'
-                            // show='https://www.agima.ru/upload/iblock/f7d/f7dfd450297a53076fd5db307bd8bb7c.mp4'
-                            // show='/src/static/video/randevu05.mp4'
-                            onPlaying={::this.videoLoaded}
+                        <ReactPlayer
+                            url='https://www.agima.ru/upload/iblock/f7d/f7dfd450297a53076fd5db307bd8bb7c.mp4'
+                            fileConfig={{attributes: {autoPlay: true, muted: true, playsInline: true, preload: false}}}
+                            playing
+                            loop
+                            playsinline
+                            volume={0}
+                            onStart={::this.videoLoaded}
                         />
                     </div>
 
@@ -56,19 +58,19 @@ export default class Slider extends Component {
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className={s.news}>
-                            <div className={s.news_wrapper}>
-                                <div className={s.title}>Последние записи</div>
-                                <div className={s.list}>
-                                    <div className={s.element}>
-                                        <div className={s.content_news}>
-                                            <a href='#'>Цифровое <i>развитие России отстаёт</i> от мирового в среднем на 5–8 лет</a>
+                            <div className={s.news}>
+                                <div className={s.news_wrapper}>
+                                    <div className={s.title}>Последние записи</div>
+                                    <div className={s.list}>
+                                        <div className={s.element}>
+                                            <div className={s.content_news}>
+                                                <a href='#'>Цифровое <i>развитие России отстаёт</i> от мирового в среднем на 5–8 лет</a>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className={s.element}>
-                                        <div className={s.content_news}>
-                                            <a href='#'><i>Минусы Битрикса</i>, или Битрикс глазами программиста</a>
+                                        <div className={s.element}>
+                                            <div className={s.content_news}>
+                                                <a href='#'><i>Минусы Битрикса</i>, или Битрикс глазами программиста</a>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
